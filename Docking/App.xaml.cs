acf77db4f90f95f747a0edc88eedb48236028f6b
Florian Koch <fkoch@selectline.de>
2016-09-15 16:23:02 +0200

    Added project

---
+++ Docking/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Docking
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        private void OnAppStartup_UpdateThemeName(Object sender, StartupEventArgs e)
+        {
+
+            DevExpress.Xpf.Core.ApplicationThemeHelper.UpdateApplicationThemeName();
+        }
+    }
+}
---
+++ Docking/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Docking
+{
+    public class HwndHostEx : HwndHost
+    {
+        internal const int
+            WS_CHILD = 0x40000000,
+            WS_VISIBLE = 0x10000000,
+            LBS_NOTIFY = 0x00000001,
+            LISTBOX_ID = 0x00000001,
+            WS_VSCROLL = 0x00200000,
+            WS_BORDER = 0x00800000;
+        [DllImport("user32.dll", EntryPoint = "CreateWindowEx", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr CreateWindowEx(int dwExStyle,
+                                              string lpszClassName,
+                                              string lpszWindowName,
+                                              int style,
+                                              int x, int y,
+                                              int width, int height,
+                                              IntPtr hwndParent,
+                                              IntPtr hMenu,
+                                              IntPtr hInst,
+                                              [MarshalAs(UnmanagedType.AsAny)] object pvParam);
+
+        internal IntPtr hwndControl;
+
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            hwndControl = CreateWindowEx(0, "listbox", "",
+                                          WS_CHILD | WS_VISIBLE | LBS_NOTIFY
+                                            | WS_VSCROLL | WS_BORDER,
+                                          10, 10,
+                                          100, 100,
+                                          hwndParent.Handle,
+                                          (IntPtr)LISTBOX_ID,
+                                          IntPtr.Zero,
+                                          0);
+
+            return new HandleRef(this, hwndControl);
+        }
+
+        protected override IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            handled = false;
+            return IntPtr.Zero;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "DestroyWindow", CharSet = CharSet.Unicode)]
+        internal static extern bool DestroyWindow(IntPtr hwnd);
+
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            DestroyWindow(hwnd.Handle);
+        }
+    }
+
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal const int
+          LB_ADDSTRING = 0x00000180;
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage", CharSet = CharSet.Unicode)]
+        internal static extern IntPtr SendMessage(IntPtr hwnd,
+                                                  int msg,
+                                                  IntPtr wParam,
+                                                  string lParam);
+
+        HwndHostEx host;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void On_UIReady(object sender, EventArgs e)
+        {
+            dockLayoutManager.DockController.Float(dockPanel);
+            host = new HwndHostEx();
+            layoutControlItem.Content = host;
+            for (int i = 0; i < 15; i++) //populate listbox
+            {
+                string itemText = "Item" + i.ToString();
+                SendMessage(host.hwndControl, LB_ADDSTRING, IntPtr.Zero, itemText);
+            }
+        }
+    }
+}
